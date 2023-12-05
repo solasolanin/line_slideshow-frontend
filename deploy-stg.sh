7979fecddacd97c4a68dd4dcc298ef37ef9d2573
hidetaka <asaokahide@yahoo.co.jp>
2023-12-06 02:14:44 +0900

    デプロイ手順作成

---
+++ deploy-stg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# シークレットの環境変数を取得
+ACCESS_KEY_ID=$(aws secretsmanager get-secret-value --secret-id line-slideshow-sm-dev --output json | jq -r '.SecretString' | jq -r '.ACCESS_KEY_ID')
+SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value --secret-id line-slideshow-sm-dev --output json | jq -r '.SecretString' | jq -r '.SECRET_ACCESS_KEY')
+
+npm run build
+npx serverless deploy --stage stg \
+    --param="ENV_AWS_ACCESS_KEY_ID=${ACCESS_KEY_ID}" \
+    --param="ENV_AWS_SECRET_ACCESS_KEY=${SECRET_ACCESS_KEY}"
